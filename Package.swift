bec609d42860bef0dccfc0c4d2e6ea9e7cb35120
Jerwin Pastoral <project@jerwinpastoral.com>
2025-05-30 18:42:34 +0800

    Remove the Kingfisher

--- Package.swift
+++ Package.swift
@@ -14,11 +14,6 @@ let package = Package(
             name: "SlideshowGallery",
             targets: ["SlideshowGallery"]),
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/onevcat/Kingfisher.git",
-            from: "8.1.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
